6061e9dbc7f1fe3d0653796c70659a0f8a4d15e8
Antonio Mejia <58567224+avmejia-tec@users.noreply.github.com>
2023-06-01 16:22:38 +0000

    Memoria

---
+++ memoria/familia.cpp
@@ -0,0 +1,26 @@
+#include "familia.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::ostream;
+
+void Familia::agregarMiembro(){
+    Hijo *h=new Hijo("Mario");
+    Hija *ha=new Hija("Elda");
+    fam.push_back(h);
+    fam.push_back(ha);
+}
+void Familia::imprimir(){
+    for (int i = 0; i < fam.size(); i++){
+        fam[i]->imprimir();
+    }
+}
+ostream & operator<<(ostream &COUT,Familia &f){
+    
+    return COUT;
+}
+
